b2e13544daec8acbfb3220666e49ddec07c34398
reiamt <tim.maier1@web.de>
2026-01-17 16:44:43 +0100

    init go module and add hello world

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/reiamt/distributed_rate_limiter
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World! Go is running on my omarchy machine <3")
+}
