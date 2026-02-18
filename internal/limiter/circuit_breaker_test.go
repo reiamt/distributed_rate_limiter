201265eb78a2f6ae18adb9b1b89e4d644b398e34
reiamt <tim.maier1@web.de>
2026-02-18 07:25:41 +0100

    finieshed unittests for circuit breaker

--- internal/limiter/circuit_breaker_test.go
+++ internal/limiter/circuit_breaker_test.go
@@ -15,7 +15,7 @@ func TestNewCircuitBreakerAllowed(t *testing.T) {
 }
 
 func TestOpenAfterThreshold(t *testing.T) {
-	circuitBreaker := NewCircuitBreaker(3, 30*time.Second)
+	circuitBreaker := NewCircuitBreaker(3, 100*time.Millisecond)
 
 	for cntr := 0; cntr < circuitBreaker.threshold; cntr++ {
 		circuitBreaker.RecordResult(false)
@@ -23,5 +23,45 @@ func TestOpenAfterThreshold(t *testing.T) {
 	allowed := circuitBreaker.Allow()
 	if allowed {
 		t.Errorf("Request should not be allowed")
+	}
+}
+
+func TestHalfOpenAfterTimeout(t *testing.T) {
+	cb := NewCircuitBreaker(3, 100*time.Millisecond)
+
+	for i := 0; i < cb.threshold; i++ {
+		cb.RecordResult(false)
+	}
+	time.Sleep(101 * time.Millisecond)
+	if !cb.Allow() {
+		t.Errorf("Request should be allowed.")
+	}
+}
+
+func TestClosesOnSuccess(t *testing.T) {
+	cb := NewCircuitBreaker(3, 100*time.Millisecond)
+
+	for i := 0; i < cb.threshold; i++ {
+		cb.RecordResult(false)
+	}
+	time.Sleep(101 * time.Millisecond)
+	// now half open
+	cb.RecordResult(true)
+	// closed
+	if !cb.Allow() {
+		t.Errorf("Request should be allowed.")
+	}
+}
+
+func TestReopensAfterFailureInHalfOpen(t *testing.T) {
+	cb := NewCircuitBreaker(3, 100*time.Millisecond)
+
+	for i := 0; i < cb.threshold; i++ {
+		cb.RecordResult(false)
+	}
+	time.Sleep(101 * time.Millisecond)
+	cb.RecordResult(false)
+	if cb.Allow() {
+		t.Errorf("Request should not be allowed.")
 	}
 }
